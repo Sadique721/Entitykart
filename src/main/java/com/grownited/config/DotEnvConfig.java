2fcef34b04935d62c7424c4a55d8c43b315b5b05
Md Sadique Amin <162752803+Sadique721@users.noreply.github.com>
2026-04-13 14:13:17 +0530

    Refactor dotenv property loading to remove defaults

--- src/main/java/com/grownited/config/DotEnvConfig.java
+++ src/main/java/com/grownited/config/DotEnvConfig.java
@@ -22,28 +22,28 @@ public class DotEnvConfig {
         Properties properties = new Properties();
         
         // Database
-        properties.setProperty("DB_HOST", dotenv.get("DB_HOST", "localhost"));
-        properties.setProperty("DB_PORT", dotenv.get("DB_PORT", "3306"));
-        properties.setProperty("DB_NAME", dotenv.get("DB_NAME", "entitykart"));
-        properties.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME", "root"));
-        properties.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", "0721"));
+        properties.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        properties.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        properties.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        properties.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        properties.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         
         // Server
-        properties.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT", "9999"));
+        properties.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         
         // Email
-        properties.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME", "mdsadiqueamin721721@gmail.com"));
-        properties.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD", "girseqjctzqpgmls"));
+        properties.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+        properties.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
         
         // Cloudinary
-        properties.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME", "dcnoffp8x"));
-        properties.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY", "563865189212327"));
-        properties.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET", "r4AUWImkWRkvcH3Fn9kDGYK6uEc"));
+        properties.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        properties.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+        properties.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
         
         // Authorize.Net
-        properties.setProperty("AUTHORIZE_NET_API_LOGIN_ID", dotenv.get("AUTHORIZE_NET_API_LOGIN_ID", "89g9AHbvJ"));
-        properties.setProperty("AUTHORIZE_NET_TRANSACTION_KEY", dotenv.get("AUTHORIZE_NET_TRANSACTION_KEY", "3c99q2SFnD92pE64"));
-        properties.setProperty("AUTHORIZE_NET_ENVIRONMENT", dotenv.get("AUTHORIZE_NET_ENVIRONMENT", "sandbox"));
+        properties.setProperty("AUTHORIZE_NET_API_LOGIN_ID", dotenv.get("AUTHORIZE_NET_API_LOGIN_ID"));
+        properties.setProperty("AUTHORIZE_NET_TRANSACTION_KEY", dotenv.get("AUTHORIZE_NET_TRANSACTION_KEY"));
+        properties.setProperty("AUTHORIZE_NET_ENVIRONMENT", dotenv.get("AUTHORIZE_NET_ENVIRONMENT"));
         
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
         configurer.setProperties(properties);
