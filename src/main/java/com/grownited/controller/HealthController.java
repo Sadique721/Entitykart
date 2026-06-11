00f297913d4f5aea6cb9416264a89d515717d8d7
Md Sadique Amin <mdsadiqueamin721786@gmail.com>
2026-06-11 07:37:18 +0530

    first commit

---
+++ src/main/java/com/grownited/controller/HealthController.java
@@ -0,0 +1,10 @@
+package com.grownited.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() { return "OK"; }
+}
