27ace8421a6b1b37b2f9cf3fa8377ccc15afd98d
Md Sadique Amin <162752803+Sadique721@users.noreply.github.com>
2026-06-11 07:46:22 +0530

    Comment out DotEnvConfig class and its methods

--- src/main/java/com/grownited/config/DotEnvConfig.java
+++ src/main/java/com/grownited/config/DotEnvConfig.java
@@ -1,54 +1,54 @@
-package com.grownited.config;
+// package com.grownited.config;
 
-import java.util.Properties;
+// import java.util.Properties;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
-import io.github.cdimascio.dotenv.Dotenv;
+// import io.github.cdimascio.dotenv.Dotenv;
 
-@Configuration
-public class DotEnvConfig {
+// @Configuration
+// public class DotEnvConfig {
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+//     @Bean
+//     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         
-        // Load .env file from root directory
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()  // Don't fail if .env doesn't exist
-                .load();
+//         // Load .env file from root directory
+//         Dotenv dotenv = Dotenv.configure()
+//                 .ignoreIfMissing()  // Don't fail if .env doesn't exist
+//                 .load();
         
-        Properties properties = new Properties();
+//         Properties properties = new Properties();
         
-        // Database
-        properties.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-        properties.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-        properties.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-        properties.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        properties.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//         // Database
+//         properties.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+//         properties.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+//         properties.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+//         properties.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//         properties.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
         
-        // Server
-        properties.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+//         // Server
+//         properties.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         
-        // Email
-        properties.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
-        properties.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+//         // Email
+//         properties.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
+//         properties.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
         
-        // Cloudinary
-        properties.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        properties.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        properties.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+//         // Cloudinary
+//         properties.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+//         properties.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+//         properties.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
         
-        // Authorize.Net
-        properties.setProperty("AUTHORIZE_NET_API_LOGIN_ID", dotenv.get("AUTHORIZE_NET_API_LOGIN_ID"));
-        properties.setProperty("AUTHORIZE_NET_TRANSACTION_KEY", dotenv.get("AUTHORIZE_NET_TRANSACTION_KEY"));
-        properties.setProperty("AUTHORIZE_NET_ENVIRONMENT", dotenv.get("AUTHORIZE_NET_ENVIRONMENT"));
+//         // Authorize.Net
+//         properties.setProperty("AUTHORIZE_NET_API_LOGIN_ID", dotenv.get("AUTHORIZE_NET_API_LOGIN_ID"));
+//         properties.setProperty("AUTHORIZE_NET_TRANSACTION_KEY", dotenv.get("AUTHORIZE_NET_TRANSACTION_KEY"));
+//         properties.setProperty("AUTHORIZE_NET_ENVIRONMENT", dotenv.get("AUTHORIZE_NET_ENVIRONMENT"));
         
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        configurer.setProperties(properties);
-        configurer.setIgnoreUnresolvablePlaceholders(false);
+//         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+//         configurer.setProperties(properties);
+//         configurer.setIgnoreUnresolvablePlaceholders(false);
         
-        return configurer;
-    }
-}
+//         return configurer;
+//     }
+// }
